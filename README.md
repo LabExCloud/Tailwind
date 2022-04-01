cdca2ac111075793f0c632d1cdcd015430372a0f
JinsoRaj <jinso.trv18it032@gecbh.ac.in>
2022-04-01 13:26:23 +0530

    basics

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Tailwind
-V2
+# TailwindCSS
+
+#### Install
+```git clone https://github.com/LabExCloud/Tailwind```
+
+``` cd Tailwind ```
+
+```npm i ```
+
+
+#### Tailwind config
+
+```npx tailwindcss init --full```
+
+```npx tailwindcss init```
+
+
+#### Tailwind css build
+```npm run build-css```
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./*/*.html"],
+  theme: {
+    extend: {
+      colors: {
+
+        mainclr: {
+          100: '#032C6A',
+          150: '#07234D',
+          200: '#040D21',
+          300: '#240626'
+        },
+        cardclr: '#090D2E',
+        borderclr: '#CEAF0E'
+
+      },
+      fontFamily: {
+        snig: ['Sniglet'],
+        rev: ['Revalia']
+      }
+    },
+  },
+  plugins: [],
+}
